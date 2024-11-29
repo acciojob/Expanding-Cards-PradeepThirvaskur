45a9600b239b22e0459bca9bdc0c4dfe73b9af44
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-29 15:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function active(event){
+    let panels = document.querySelectorAll('.panel'); 
+    // Remove the "active","show" class from all panels
+
+    panels.forEach((panel) => panel.classList.remove('active'));
+    panels.forEach((show) => show.classList.remove('show'));
+    // Add the "active","show" class to the clicked panel
+
+    event.target.classList.add('active');
+    event.target.classList.add('show');
+}
